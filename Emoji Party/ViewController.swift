8ed8db65e574d0a3eed21a4405d09ab098851cb8
Thinker Pal <thinkerpal@gmail.com>
2018-07-28 09:28:15 +0800

    Initial Commit

---
+++ Emoji Party/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emoji Party
+//
+//  Created by Rui Yang Tan on 28/7/18.
+//  Copyright © 2018 Rui Yang Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
